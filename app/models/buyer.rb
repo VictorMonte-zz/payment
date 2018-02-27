9183492e0d96c59f30527cc10306c8c424304eb7
Victor Monte <victorbcm@hotmail.com>
2018-02-26 23:45:15 -0300

    Add buyer validations

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -2,4 +2,10 @@ class Buyer < ActiveRecord::Base
   has_many :cards
   has_many :credit_card_payments
   has_many :boleto_payments
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false  }, format: { with: VALID_EMAIL_REGEX }
+  validates :name, presence: true
+  validates :cpf, presence: true
+
 end
