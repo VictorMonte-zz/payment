4464593469f19635447e5403fc0536d7e7dd8d2a
Victor Monte <victorbcm@hotmail.com>
2018-02-26 23:41:43 -0300

    Alter card migration columns

--- db/migrate/20180220040608_create_cards.rb
+++ db/migrate/20180220040608_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.1]
   def change
     create_table :cards do |t|
-      t.string :holdername
-      t.integer :number
-      t.date :expirationDate
+      t.string :holder_name
+      t.string :number
+      t.date :expiration_date
       t.integer :cvv
     end
   end
