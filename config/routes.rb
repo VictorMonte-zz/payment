507a5a4c104bb71697b7f4ca4cbbb43d506a0dd7
Victor Monte <victorbcm@hotmail.com>
2018-03-01 00:13:36 -0300

    Add route for creditcard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   post '/buyers', to:'buyers#create'
-
+  post '/buyers/:cpf/credit-cards', to:'credit_cards#create'
   post '/payments', to: 'payments#create'
 end
