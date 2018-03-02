4faabd408f69def5da50c4d6298d13417bced5f9
Victor Monte <victorbcm@hotmail.com>
2018-03-02 00:40:23 -0300

    Improve create credit card

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,15 +1,17 @@
-require_relative '../services/credit_card_service'
+require_relative '../services/create_credit_card'
 
 class CreditCardsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    @card = CreditCardService.new(credit_card_params).create(params[:cpf])
-    if @card.valid?
-      render json: @card.id, head: :ok
-    else
+    @card = create_credit_card.call
+
+    if !@card.valid?
       render json: @card.errors.full_messages, status: :bad_request
     end
+
+    render json: @card.id, head: :ok
+
   end
 
   private
@@ -17,5 +19,9 @@ class CreditCardsController < ApplicationController
   def credit_card_params
     params.require(:cpf)
     params.require(:card).permit(:holder_name, :number, :expiration_date, :cvv)
+  end
+
+  def create_credit_card
+    create_credit_card ||= CreateCreditCard.new(credit_card_params, params[:cpf])
   end
 end
