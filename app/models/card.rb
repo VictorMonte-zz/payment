13a2774bf852be3fb894524ef5e548ef2c9e8b12
Victor Monte <victorbcm@hotmail.com>
2018-03-02 22:36:12 -0300

    Test credit card payment

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,6 @@
 class Card < ActiveRecord::Base
   belongs_to :buyer
+  belongs_to :credit_card_payment, optional: true
   
   validates :holder_name, presence: true
   validates :number, presence: true, length: { is: 16  }
