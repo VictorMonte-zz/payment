5b860090b7919d910983d8db36ab60494e0fd717
Victor Monte <victorbcm@hotmail.com>
2018-03-02 22:05:53 -0300

    Fix boleto association

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,9 @@
 class Payment < ActiveRecord::Base
   belongs_to :buyer, optional: true 
   belongs_to :client, optional: true
+  
+  has_one :boleto_payment
+  has_one :credit_card_payment
 
   validates :amount, presence: true
   validates :client_id, presence: true
